7927ebe29124b84c8fcabff04b7ff692cff0881a
wlee221 <wlee221@live.unc.edu>
2020-11-30 13:04:31 -0800

    Add 7301 repro

---
+++ samples/react/7301-predictions-configure/src/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+import Amplify from "aws-amplify";
+import { Predictions, AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+Predictions.addPluggable(new AmazonAIPredictionsProvider());
+
+function App() {
+  useEffect(() => {
+    Predictions.identify({
+      labels: {
+        source: {
+          key: "skateboard.jpg",
+        },
+        type: "ALL",
+      },
+    })
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
