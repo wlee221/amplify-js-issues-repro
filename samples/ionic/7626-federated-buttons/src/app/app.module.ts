351bccaab0be7915ea85a65e058ce587b5e887d0
wlee221 <wlee221@live.unc.edu>
2021-02-10 14:29:30 -0800

    Add ionic sample for custom federated button

---
+++ samples/ionic/7626-federated-buttons/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+@NgModule({
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [
+		AmplifyUIAngularModule,
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+	],
+	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
